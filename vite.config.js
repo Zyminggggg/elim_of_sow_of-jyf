2dd88c18d17e75ee108a87e52158b891a5122aba
Zymingggg <Zymingggg@163.com>
2025-12-29 12:39:42 +0800

    提交所有已跟踪文件的修改

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Vercel 部署通常不需要配 base，或者配成 '/'
+  base: '/',
 })
